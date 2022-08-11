f2c5be7815fa336ef93a085ad7b5201a1cf913c0
marc-bentoy <marc.bentoy@gmail.com>
2022-08-11 17:56:21 +0800

    added the Record model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_095151) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_095606) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.integer "artist_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_records_on_artist_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_095151) do
   end
 
   add_foreign_key "artists", "users"
+  add_foreign_key "records", "artists"
+  add_foreign_key "records", "users"
 end
