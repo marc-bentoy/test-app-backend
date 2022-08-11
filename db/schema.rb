0312d36f7bc51d700fa8bcf1a90a63de6e261183
marc-bentoy <marc.bentoy@gmail.com>
2022-08-11 17:52:14 +0800

    added back the Artist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_094616) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_095151) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_094616) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "artists", "users"
 end
